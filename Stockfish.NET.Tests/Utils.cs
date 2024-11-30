b4b108459db7cc150b1c973efa2f116f1e836a8e
Dracose <Dracose12@gmail.com>
2024-11-30 14:22:24 +0000

    Revert Stockfish directory location

--- Stockfish.NET.Tests/Utils.cs
+++ Stockfish.NET.Tests/Utils.cs
@@ -6,7 +6,23 @@ namespace Stockfish.NET.Tests
     {
         public static string GetStockfishDir()
         {
-            string path = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform( System.Runtime.InteropServices.OSPlatform.Windows) ? $@"C:\stockfish-windows-x86-64-avx2" : "/usr/games/stockfish";
+            var assembly = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SingleOrDefault(a => a.GetName().Name == "Stockfish.NET");
+            var location = assembly?.Location;
+            var dir = Directory.GetParent(Directory.GetParent(Directory
+                .GetParent(Directory.GetParent(Directory.GetParent(location).ToString())
+                    .ToString()).ToString()).ToString());
+            Console.WriteLine(dir);
+            string path = null;
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                path = $@"{dir}\Stockfish.NET.Tests\Stockfish\win\stockfish_12_win_x64\stockfish_20090216_x64.exe";
+            }
+            else
+            {
+                path = "/usr/games/stockfish";
+            }
             return path;
         }
 
